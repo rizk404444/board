2b5cde7be6af13fcf20a8af54208bff2949c12e5
rizk <rizk303333@gmail.com>
2020-06-16 17:34:44 +0900

    보드 DB주고 받기랑 조회가능

---
+++ study/src/main/java/kr/co/dao/BoardDAOImpl.java
@@ -0,0 +1,29 @@
+package kr.co.dao;
+
+import java.util.List;
+
+import javax.inject.Inject;
+
+import org.apache.ibatis.session.SqlSession;
+import org.springframework.stereotype.Repository;
+
+import kr.co.vo.BoardVO;
+
+@Repository
+public class BoardDAOImpl implements BoardDAO {
+	
+	@Inject
+	private SqlSession sqlSession;
+	
+	@Override
+	public void write(BoardVO boardVO) throws Exception {
+		sqlSession.insert("boardMapper.insert", boardVO);
+	}
+	// 게시물 목록 조회
+		@Override
+		public List<BoardVO> list() throws Exception {
+		
+			return sqlSession.selectList("boardMapper.list");
+
+		}
+}
